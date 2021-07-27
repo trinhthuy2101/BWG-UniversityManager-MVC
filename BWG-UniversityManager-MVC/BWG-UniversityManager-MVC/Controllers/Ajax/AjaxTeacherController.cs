48f777cefbe7ffbc11f19336132af9af3a069e0b
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-07-27 13:35:39 +0700

    test doing with teacher
    test, try to do some modification

--- BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using ASP_NET_MVC.Controllers.Ajax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP_NET_MVC.Models;
+
 
 namespace ASP_NET_MVC.Controllers.UniversityManager
 {
-    public class AjaxTeacherController : Controller
+    public class AjaxTeacherController : AjaxController
     {
         // GET: AjaxTeacher
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetTimeTable(string id) {
+            
+            string SQL = "select * from Course where Teacher='" + id + "'";
+            return Json(DB.Database.SqlQuery<Course>(SQL).ToList(),JsonRequestBehavior.AllowGet);
+        }
     }
+
 }
