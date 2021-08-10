2b49edd39aea9d16db97b3f0072d1a516fc7c47b
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-08-10 21:35:10 +0700

    teacher timetable and give points

--- BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
@@ -6,21 +6,39 @@ using System.Web;
 using System.Web.Mvc;
 using ASP_NET_MVC.Models;
 
-
 namespace ASP_NET_MVC.Controllers.UniversityManager
 {
-    public class AjaxTeacherController : AjaxController
+    public class AjaxTeacherController : BaseController
     {
-        // GET: AjaxTeacher
+        //TODO: authorize teacher, admin
+        //[Authorize(Roles = "HRManager,Finance")]
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetTimeTable(string id) {
-            
+        //[Authorize(Roles = "HRManager,Finance")]
+        public ActionResult GetTimeTable() {
+            string accountid = LoginModel.Id;
+            string SQLFindTrueID=" select UserName from Account where id = '" + accountid + "'";
+            string id = DB.Database.SqlQuery<string>(SQLFindTrueID).FirstOrDefault();
+
             string SQL = "select * from Course where Teacher='" + id + "'";
             return Json(DB.Database.SqlQuery<Course>(SQL).ToList(),JsonRequestBehavior.AllowGet);
         }
+        public ActionResult GetListRegisteredCourseFromRegisteredCourse(string courseid) {
+            string SQL = "select RegisteredCourse.Course as Course,RegisteredCourse.Student as Id,RegisteredCourse.Point as Point,Student.Name as Student from webt2289_thuy.RegisteredCourse , webt2289_thuy.Student where Course = '"+ courseid + "' and Student.id = RegisteredCourse.Student";
+            var list = DB.Database.SqlQuery<StudentWithPointAndCourse>(SQL).ToList();
+            if (list.Count <= 0) return Json("cannot get", JsonRequestBehavior.AllowGet);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SavePoints(string id,string point) {
+            string SQL= "UPDATE RegisteredCourse  SET Point =" + point+" WHERE Student='"+id+"'";
+            DB.Database.ExecuteSqlCommand(SQL);
+            return Json(new object(), JsonRequestBehavior.AllowGet);
+
+        }
+
+
     }
 
 }
---
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Models/StudentWithPointAndCourse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    public class StudentWithPointAndCourse
+    {
+        public string Course { get; set; }
+        public string Id { get; set; }
+        public Nullable<int> Point { get; set; }
+        public string Student { get; set; }
+
+    }
+}
