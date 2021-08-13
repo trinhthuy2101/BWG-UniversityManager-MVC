33650cf9c64805a730b72b0361c4205f84ec796a
DinhHuynhTienPhu <DinhHuynhTienPhu@users.noreply.github.com>
2021-08-13 12:03:52 +0700

    Student register course

--- BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxStudentForStudentController.cs
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxStudentForStudentController.cs
@@ -27,6 +27,43 @@ namespace ASP_NET_MVC.Controllers.Ajax
             string SQL = "select * from Course where id in (select Course from RegisteredCourse where Student='"+id +"')";
             return Json(DB.Database.SqlQuery<Course>(SQL).ToList(), JsonRequestBehavior.AllowGet); 
         }
+        public ActionResult ShowCoursesToRegister()
+        {
+            string accountid = LoginModel.Id;
+            string SQLFindTrueID = " select UserName from Account where id = '" + accountid + "'";
+            string id = DB.Database.SqlQuery<string>(SQLFindTrueID).FirstOrDefault();
+
+
+            string SQL = "select * from Course ";
+            var courses = DB.Database.SqlQuery<Course>(SQL).ToList();
+
+            string SQLregisteredCourse = "select * from Course where id in (select Course from RegisteredCourse where Student='" + id + "')";
+            var registered = DB.Database.SqlQuery<Course>(SQLregisteredCourse).ToList();
+            if (registered.Count != 0||registered!=null)
+            {
+                for (int i = 0; i < courses.Count; i++)
+                {
+                    if (registered.FindAll(x => x.Subject == courses[i].Subject).Count != 0)
+                    {
+                        courses.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            return Json(courses, JsonRequestBehavior.AllowGet); 
+        }
+        public ActionResult RegisterToCourse(string course)
+        {
+            string accountid = LoginModel.Id;
+            string SQLFindTrueID = " select UserName from Account where id = '" + accountid + "'";
+            string studentid = DB.Database.SqlQuery<string>(SQLFindTrueID).FirstOrDefault();
+
+            
+
+            string SQL = "insert into RegisteredCourse values('"+course+"','"+studentid+"',NULL) ";
+            DB.Database.ExecuteSqlCommand(SQL);
+            return (Json("Registered", JsonRequestBehavior.AllowGet));
+        }
 
         ////get list registerdcourses
         //public ActionResult GetListRegisteredCourseFromRegisteredCourse(string courseid)
