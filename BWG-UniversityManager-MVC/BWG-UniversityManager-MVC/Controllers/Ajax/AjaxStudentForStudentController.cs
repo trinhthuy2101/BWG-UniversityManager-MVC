44ba760d2abf2efdfbe2e2b3e200096fbe6dd189
Thang Dang <dangthang013@gmail.com>
2021-08-13 20:19:43 +0700

    auto redirect after login

--- BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxStudentForStudentController.cs
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxStudentForStudentController.cs
@@ -6,10 +6,16 @@ using System.Web.Mvc;
 
 namespace ASP_NET_MVC.Controllers.Ajax
 {
-    public class AjaxStudentForStudentController : Controller
+    public class AjaxStudentForStudentController : BaseController
     {
         // GET: AjaxStudentForStudent
         public ActionResult Index()
+        {
+            if (LoginModel.Role == "Teacher")
+                return View();
+            else return RedirectToAction("NoPermission", "AjaxStudentForStudent");
+        }
+        public ActionResult NoPermission()
         {
             return View();
         }
--- BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Controllers/Ajax/AjaxTeacherController.cs
@@ -14,6 +14,12 @@ namespace ASP_NET_MVC.Controllers.UniversityManager
         //[Authorize(Roles = "HRManager,Finance")]
         public ActionResult Index()
         {
+            if (LoginModel.Role == "Teacher")
+                return View();
+            else return RedirectToAction("NoPermission", "AjaxTeacher");
+        }
+        public ActionResult NoPermission()
+        {
             return View();
         }
         //[Authorize(Roles = "HRManager,Finance")]
