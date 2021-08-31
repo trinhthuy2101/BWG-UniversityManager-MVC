80d0e702e53e7c4cef6274de4fefded78925cff8
trinhthuy2101 <67819268+trinhthuy2101@users.noreply.github.com>
2021-08-31 23:39:58 +0700

    Thuy-Admin-ShowList/Add/DeleteCourse

---
+++ BWG-UniversityManager-MVC/BWG-UniversityManager-MVC/Models/ViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    public class ViewModels
+    {
+        public List<Course> ViewModelCourse { get; set; }
+        public List<Teacher> ViewModelTeacher { get; set; }
+        public List<Subject> ViewModelSubject { get; set; }
+        public List<Room> ViewModelRoom { get; set; }
+    }
+}
